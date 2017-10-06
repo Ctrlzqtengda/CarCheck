92ea59ae9842fd11a5ac87d44b81e6cd06844005
Yuezongshen <zongshen888@gmail.com>
2017-10-06 11:33:11 +0800

    test

--- CarCheck/AppDelegate.m
+++ CarCheck/AppDelegate.m
@@ -20,7 +20,7 @@
 
 @implementation AppDelegate
 
-
+//tttttttest
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
